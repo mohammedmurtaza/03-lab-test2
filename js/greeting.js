c3916fcf6807dc64b56af1cd7d46d319d4793bba
Mohammed Murtaza <p14155763@dmu.ac.uk>
2017-10-19 16:48:45 +0100

    lab work

--- js/greeting.js
+++ js/greeting.js
@@ -3,9 +3,12 @@
   let myName = document.getElementById("my-name"); 
   let userName = document.getElementById("user-name"); 
   let getName = document.getElementById("get-name");
+  let userHeader = document.getElementById("my-header");
 
   function PerformGreeting(ev) {
     myName.innerHTML = userName.value;
+	userHeader.innerHTML = userName.value;
+	userHeader.classList.add =("header-background");
     ev.preventDefault();
   }
 
